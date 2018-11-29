16022ff86c222dca5b2d290e9db540ab94541289
Prakash <dusejapt@gmail.com>
2018-11-29 13:18:43 +0000

    common code

--- src/main/java/com/ccsbi/co/common/api/repository/CountryRepo.java
+++ src/main/java/com/ccsbi/co/common/api/repository/CountryRepo.java
@@ -2,6 +2,8 @@ package com.ccsbi.co.common.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CountryRepo extends JpaRepository<T, ID> {
+import com.ccsbi.co.common.api.repository.entity.Countries;
+
+public interface CountryRepo extends JpaRepository<Countries, Long> {
 
 }
--- src/main/java/com/ccsbi/co/common/api/repository/entity/Countries.java
+++ src/main/java/com/ccsbi/co/common/api/repository/entity/Countries.java
@@ -1,11 +1,31 @@
 package com.ccsbi.co.common.api.repository.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @SuppressWarnings("serial")
 @Entity(name = "countries")
 public class Countries implements Serializable{
 
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "countryname")
+	private String countryName;
+	
+	@Column(name = "countrycode")
+	private String countryCode;
+	
+	@Column(name = "sysdate")
+	private Date sysDate;
+	
+	@Column(name = "active")
+	private String active;
 }
