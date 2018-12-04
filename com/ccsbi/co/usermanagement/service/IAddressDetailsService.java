65a3fa193c52ab9305a27915cf16a8a5e9f3404f
pduseja <dusejapt@gmail.com>
2018-12-04 11:53:57 +0000

    Delete IAddressDetailsService.java
    wrong location

--- com/ccsbi/co/usermanagement/service/IAddressDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.ccsbi.co.usermanagement.service;
-
-import com.ccsbi.co.usermanagement.service.model.AddressDetails;
-
-public interface IAddressDetailsService {
-	
-	AddressDetails save(AddressDetails addressDetails);
-
-}
