c7e1a22a24a4c801b000467215bfd600baf52386
pduseja <dusejapt@gmail.com>
2018-12-04 11:54:52 +0000

    Delete IUsersDetailsService.java
    wrong location

--- com/ccsbi/co/usermanagement/service/IUsersDetailsService.java
+++
@@ -1,8 +0,0 @@
-package com.ccsbi.co.usermanagement.service;
-
-import com.ccsbi.co.usermanagement.service.model.UsersDetails;
-
-public interface IUsersDetailsService {
-
-	public UsersDetails save(UsersDetails userDetails);
-}
