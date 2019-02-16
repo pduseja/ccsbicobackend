978bddcf48ffdf3def0d4dded559d6d1530b55b8
pduseja <dusejapt@gmail.com>
2019-02-16 16:12:20 +1100

    Code for Secure message for Communication Dashboard

--- src/main/java/com/ccsbi/co/usermanagement/repository/IMessageFollowUpRepo.java
+++ src/main/java/com/ccsbi/co/usermanagement/repository/IMessageFollowUpRepo.java
@@ -1,8 +1,10 @@
 package com.ccsbi.co.usermanagement.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,5 +14,13 @@ public interface IMessageFollowUpRepo extends JpaRepository<IMessageFollowUp, Lo
 	
 	@Query("select imfp from imessagefollowup imfp where imfp.iMessage.iMessageId=:iMessageId")
 	List<IMessageFollowUp> getIMessageFollowUpList(@Param("iMessageId") int iMessageId);
+	
+	@Modifying
+	@Query("update imessagefollowup imfp set imfp.messageReply=:messageReply, imfp.responseStatus=:responseStatus,"
+			+ "imfp.readStatus=:readStatus, imfp.modBy=:modBy, imfp.modDate=:modDate,imfp.fileContent=:fileContent"
+			+ " where imfp.iMessageFollowUpId=:iMessageFollowUpId")
+	int updateiMessageFollowUp(@Param("messageReply") String messageReply,@Param("responseStatus") String responseStatus,
+			@Param("readStatus") String readStatus,@Param("modBy") String modBy,@Param("modDate") Date modDate,@Param("fileContent") byte[] fileContent,
+			@Param("iMessageFollowUpId") int iMessageFollowUpId);
 
 }
--- src/main/java/com/ccsbi/co/usermanagement/repository/IMessageRepo.java
+++ src/main/java/com/ccsbi/co/usermanagement/repository/IMessageRepo.java
@@ -1,8 +1,10 @@
 package com.ccsbi.co.usermanagement.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,4 +17,14 @@ public interface IMessageRepo extends JpaRepository<IMessage, Long> {
 	
 	@Query("SELECT im from imessage im where im.department=:department")
 	List<IMessage> getMessageList(@Param("department") String department);
+	
+	@Modifying
+	@Query("update imessage im set im.messageReply=:messageReply, im.responseStatus=:responseStatus,"
+			+ "im.readStatus=:readStatus, im.modBy=:modBy, im.modDate=:modDate,im.fileContent=:fileContent"
+			+ " where im.iMessageId=:iMessageId")
+	int updateiMessage(@Param("messageReply") String messageReply,@Param("responseStatus") String responseStatus,
+			@Param("readStatus") String readStatus,@Param("modBy") String modBy,
+			@Param("modDate") Date modDate,@Param("fileContent") byte[] fileContent,
+			@Param("iMessageId") int iMessageId);
+
 }
