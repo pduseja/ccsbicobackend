162e59980f3977297b77d1600769495f0e033069
pduseja <dusejapt@gmail.com>
2019-02-24 19:06:14 +1100

    API's for department and Subject

--- src/main/java/com/ccsbi/co/usermanagement/repository/SystemParamsRepo.java
+++ src/main/java/com/ccsbi/co/usermanagement/repository/SystemParamsRepo.java
@@ -1,5 +1,7 @@
 package com.ccsbi.co.usermanagement.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -18,5 +20,14 @@ public interface SystemParamsRepo extends JpaRepository<SystemParams, Long> {
 	
 	@Query("select sp.param3 from systemparams sp where sp.keyVal1=:keyVal1 and sp.keyVal2=:keyVal2 and sp.keyVal3=:keyVal3")
 	String getcontactUsDepatment2(@Param("keyVal1") String keyVal1,@Param("keyVal2") String keyVal2,@Param("keyVal3") String keyVal3);
+			
+	@Query("select sp.param1 from systemparams sp where sp.keyVal1=:keyVal1 and sp.keyVal2=:keyVal2 and sp.keyVal3=:keyVal3 and sp.columnOrderBy=:columnOrderBy")
+	String getMsgAboutUs(@Param("keyVal1") String keyVal1,@Param("keyVal2") String keyVal2,@Param("keyVal3") String keyVal3,@Param("columnOrderBy") int columnOrderBy);
+	
+	@Query("select sp.param2 from systemparams sp where sp.keyVal1=:keyVal1 and sp.keyVal2=:keyVal2 and sp.keyVal3=:keyVal3 and sp.columnOrderBy=:columnOrderBy")
+	String getMsgAboutUs1(@Param("keyVal1") String keyVal1,@Param("keyVal2") String keyVal2,@Param("keyVal3") String keyVal3,@Param("columnOrderBy") int columnOrderBy);
+	
+	@Query("select sp.param3 from systemparams sp where sp.keyVal1=:keyVal1 and sp.keyVal2=:keyVal2 and sp.keyVal3=:keyVal3 and sp.columnOrderBy=:columnOrderBy")
+	String getMsgAboutUs2(@Param("keyVal1") String keyVal1,@Param("keyVal2") String keyVal2,@Param("keyVal3") String keyVal3,@Param("columnOrderBy") int columnOrderBy);
 	
 }
