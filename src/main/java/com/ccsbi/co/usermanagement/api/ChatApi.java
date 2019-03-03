864203a0dc1af41c01a401ce050eedde4d17d264
pduseja <dusejapt@gmail.com>
2019-03-03 22:43:32 +1100

    Added API's for Chat

--- src/main/java/com/ccsbi/co/usermanagement/api/ChatApi.java
+++ src/main/java/com/ccsbi/co/usermanagement/api/ChatApi.java
@@ -1,20 +1,26 @@
 package com.ccsbi.co.usermanagement.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.StringUtils;
 import org.dozer.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ccsbi.co.usermanagement.client.entity.LiveChat;
 import com.ccsbi.co.usermanagement.client.entity.LiveChatMembers;
 import com.ccsbi.co.usermanagement.service.IChatService;
 
@@ -71,6 +77,58 @@ public class ChatApi {
 			return ResponseEntity.badRequest().build();
 		}
 
+	}
+	
+	@ApiOperation(value = "Get The Queue Number", notes = "Get The Queue Number", nickname = "Get The Queue Number")
+	@ApiResponses({ @ApiResponse(code = 200, message = "Success!"),
+			@ApiResponse(code = 404, message = "Page not found") })
+	@GetMapping(path = "/liveChat/{department}", produces = {
+			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public List<LiveChat> getlivechatAsPerDepartment(
+			@ApiParam(value = "", required = true) @PathVariable String department) {
+
+		List<LiveChat>  list = convertLCClientList(chatService.getlivechatAsPerdepartment(department));
+		if (list.isEmpty()) {
+			return new ArrayList<LiveChat>();
+		} else {
+			return list;
+		}
+
+	}
+
+
+	@ApiOperation(value = "Get The Queue Number", notes = "Get The Queue Number", nickname = "Get The Queue Number")
+	@ApiResponses({ @ApiResponse(code = 200, message = "Success!"),
+			@ApiResponse(code = 404, message = "Page not found") })
+	@PostMapping(path = "/liveChatRequest", produces = {
+			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public ResponseEntity<LiveChat> getLivechatRequestQueue(@ApiParam(value = "", required = true) @RequestBody LiveChat liveChat) {
+
+		LiveChat  list = convertLCClientList(chatService.getLivechatRequestQueue(convertLC(liveChat)));
+		if (list.getLiveChatId()>0) {
+			return new ResponseEntity<>(list, HttpStatus.OK);
+		} else {
+			return ResponseEntity.badRequest().build();
+		}
+
+	}
+
+	private LiveChat convertLCClientList(
+			com.ccsbi.co.usermanagement.service.model.LiveChat livechatRequestQueue) {
+		
+		return dozerMapper.map(livechatRequestQueue, LiveChat.class);
+	}
+
+	private com.ccsbi.co.usermanagement.service.model.LiveChat convertLC(LiveChat liveChat) {
+		
+		return dozerMapper.map(liveChat, com.ccsbi.co.usermanagement.service.model.LiveChat.class);
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<LiveChat> convertLCClientList(
+			List<com.ccsbi.co.usermanagement.service.model.LiveChat> getlivechatAsPerdepartment) {
+		List<LiveChat> list = new ArrayList<>();
+		return (List<LiveChat>)dozerMapper.map(getlivechatAsPerdepartment, list.getClass());
 	}
 
 	private LiveChatMembers convertModel(com.ccsbi.co.usermanagement.service.model.LiveChatMembers updateQueueNumber) {
