322ff70cb92e6e25ef4f487ff780a2cffaf147ee
pduseja <dusejapt@gmail.com>
2019-03-09 01:17:20 +1100

    Added Logic to update Queue number for Support team

--- src/main/java/com/ccsbi/co/usermanagement/repository/LiveChatRepo.java
+++ src/main/java/com/ccsbi/co/usermanagement/repository/LiveChatRepo.java
@@ -3,6 +3,7 @@ package com.ccsbi.co.usermanagement.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,4 +19,8 @@ public interface LiveChatRepo extends JpaRepository<LiveChat, Long> {
 	
 	@Query("SELECT lc from livechat lc where lc.department like concat(:department,'%') and lc.status=:status order by lc.department")
 	List<LiveChat> getLiveChatQueueNumber(@Param("department") String department,@Param("status") String status);
+	
+	@Modifying
+	@Query("update livechat lc set lc.queue=:queue where lc.liveChatId=:liveChatId")
+	int  updateQueue(@Param("queue")int queue,@Param("liveChatId") int liveChatId);
 }
